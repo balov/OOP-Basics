c326b777743dfafc6273bb6277679a49d5f9dff8
Aleksandar Balov <alexandre.balov@gmail.com>
2017-10-01 07:01:31 -0700

    Add files via upload

---
+++ 3.InheritanceExercises/InheritanceExercises/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InheritanceExercises
+{
+    internal class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            //1 Person
+            //string name = Console.ReadLine();
+            //int age = int.Parse(Console.ReadLine());
+            //
+            //try
+            //{
+            //    Child child = new Child(name, age);
+            //    Console.WriteLine(child);
+            //}
+            //catch (ArgumentException ae)
+            //{
+            //    Console.WriteLine(ae.Message);
+            //}
+
+            //2 BookShop
+            try
+            {
+                string author = Console.ReadLine();
+                string title = Console.ReadLine();
+                decimal price = decimal.Parse(Console.ReadLine());
+
+                Book book = new Book(author, title, price);
+                GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+                Console.WriteLine(book);
+                Console.WriteLine(goldenEditionBook);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
