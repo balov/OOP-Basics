97362d677287769261377140bf4fea5c21dacb40
Aleksandar Balov <alexandre.balov@gmail.com>
2017-10-01 06:59:17 -0700

    Add files via upload

---
+++ 3.InheritanceLab/InheritanceLab/Entities/StackOfStrings.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public StackOfStrings()
+    {
+        this.data = new List<string>();
+    }
+
+    public void Push(string item)
+    {
+        data.Add(item);
+    }
+
+    public string Pop()
+    {
+        string item = data[data.Count - 1];
+        data.RemoveAt(data.Count - 1);
+        return item;
+    }
+
+    public string Peek()
+    {
+        return data[data.Count - 1];
+    }
+
+    public bool IsEmpty()
+    {
+        if (data.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
